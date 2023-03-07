57124f4825e71f15cd42978b24d2e0a31161cea3
70111138 <70111138@uol.pk>
2023-03-07 15:17:15 +0500

    first comit

---
+++ Sum_of_numbers.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+using namespace std;
+int num, num1, sum;
+int main(){
+	
+	cout<<"Entre 1st number"<<endl;
+	cin>>num;
+	cout<<"Entre 2nd number "<<endl;
+	cin>>num1;
+	sum= num+num1;
+	cout<<"Sum =" <<sum<<endl;
+}
